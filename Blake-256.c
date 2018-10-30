b3bc969a2061cb998ca2a3985df235cec1b5d22c
Michu4000 <michal-kr@o2.pl>
2018-10-30 12:41:00 +0100

    Update Blake-256.c
    refactor

--- Blake-256.c
+++ Blake-256.c
@@ -2,7 +2,7 @@
 #include<string>
 #include<cstdlib>
 
-const unsigned long int con[16] = //stale c
+const unsigned long int con[16] = //constants c
 {
 	0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344,
 	0xa4093822, 0x299f31d0, 0x082efa98, 0xec4e6c89,
@@ -10,7 +10,7 @@ const unsigned long int con[16] = //stale c
 	0xc0ac29b7, 0xc97c50dd, 0x3f84d5b5, 0xb5470917
 };
 
-const int permutacja[10][16] =
+const int permutation[10][16] =
 {
 	{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
 	{ 14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3 },
@@ -24,32 +24,32 @@ const int permutacja[10][16] =
 	{ 10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13 , 0 }
 };
 
-const unsigned long int s[4] = {};//sol
+const unsigned long int s[4] = {}; //salt
 
-unsigned long int rot(unsigned long int a, int ile)//rotacja bitowa w prawo
+unsigned long int rot(unsigned long int a, int n) //rotation of n bits towards less significant bits
 {
-	return ( (a << (32 - ile)) | (a >> ile) );
+	return ( (a << (32 - n)) | (a >> n) );
 }
 
-void ladujWiadomosc(unsigned long int m[], char *wsk, unsigned long long int dl)
+void loadMsg(unsigned long int m[], char *ptr, unsigned long long int len)
 {
 	char tempchar = 0;
 
-	for (int i = 0; i < (long double)dl / 8; i++)
+	for (int i = 0; i < (long double)len / 8; i++)
 	{
 		for (int j = 0; j < 8; j++)
 		{
-			if (i * 8 + j > dl - 1)
+			if (i * 8 + j > len - 1)
 				break;
 
-			tempchar = wsk[i * 8 + j];
+			tempchar = ptr[i * 8 + j];
 			m[i] += strtoul(&tempchar, NULL, 16);
-			if (j != 7 && i * 8 + j <= dl - 1) m[i] = m[i] << 4;
+			if (j != 7 && i * 8 + j <= len - 1) m[i] = m[i] << 4;
 		}
 	}
 }
 
-void Inicjalizacja(unsigned long int v[], unsigned long int h[], unsigned long int t[])
+void initialization(unsigned long int v[], unsigned long int h[], unsigned long int t[])
 {
 	for (int i = 0; i <= 7; i++)
 		v[i] = h[i];
@@ -63,26 +63,26 @@ void Inicjalizacja(unsigned long int v[], unsigned long int h[], unsigned long i
 
 void G(unsigned long int &a, unsigned long int &b, unsigned long int &c, unsigned long int &d, int r, int i, unsigned long int m[])
 {
-	a = ( a + b + (m[ permutacja[r % 10][2 * i] ] ^ con[ permutacja[r % 10][2 * i + 1] ]) ) % 4294967296; //mod 2^32
+	a = ( a + b + (m[ permutation[r % 10][2 * i] ] ^ con[ permutation[r % 10][2 * i + 1] ]) ) % 4294967296; //mod 2^32
 	d = rot( (d ^ a), 16 );
 	c = (c + d) % 4294967296;
 	b = rot( (b ^ c), 12 );
-	a = (a + b + (m[permutacja[r % 10][2 * i + 1]] ^ con[permutacja[r % 10][2 * i]])) % 4294967296;
+	a = (a + b + (m[permutation[r % 10][2 * i + 1]] ^ con[permutation[r % 10][2 * i]])) % 4294967296;
 	d = rot((d ^ a), 8);
 	c = (c + d) % 4294967296;
 	b = rot((b ^ c), 7);
 }
 
-void Rundy(unsigned long int v[], unsigned long int m[])
+void rounds(unsigned long int v[], unsigned long int m[])
 {
 	for (int r = 0; r < 14; r++)
 	{
-		//kolumny
+		//columns
 		G(v[0], v[4], v[8], v[12], r, 0, m);
 		G(v[1], v[5], v[9], v[13], r, 1, m);
 		G(v[2], v[6], v[10], v[14], r, 2, m);
 		G(v[3], v[7], v[11], v[15], r, 3, m);
-		// przekatne
+		//diagonals
 		G(v[0], v[5], v[10], v[15], r, 4, m);
 		G(v[1], v[6], v[11], v[12], r, 5, m);
 		G(v[2], v[7], v[8], v[13], r, 6, m);
@@ -90,18 +90,19 @@ void Rundy(unsigned long int v[], unsigned long int m[])
 	}
 }
 
-void Finalizacja(unsigned long int v[], unsigned long int h[])
+void finalization(unsigned long int v[], unsigned long int h[])
 {
 	for (int i = 0; i <= 7; i++)
 		h[i] = h[i] ^ s[i%4] ^ v[i] ^ v[i + 8];
 }
 
-void wyswietlWynik(unsigned long int h[])
+void showResult(unsigned long int h[])
 {
 	std::cout << std::hex << std::uppercase;
 
+	//add zeros before number if it's too small
 	for (int i = 0; i <= 7; i++)
-	{//dopisuje zera przed liczba jesli jest za mala
+	{
 		if (h[i] >= 0x10000000)
 			std::cout << h[i];
 		else if (h[i] >= 0x1000000)
@@ -137,87 +138,87 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		std::cout << "BLAD! Podaj dokladnie jeden argument." << std::endl;
+		std::cout << "ERROR! Enter only one argument." << std::endl;
 		return 0;
 	}
 
-	char *wsk = argv[1];
+	char *ptr = argv[1];
 
-	//sprawdzam dlugosc wiadomosci (ilosc znakow)
-	unsigned long long int dl = 0;
-	while (wsk[dl] != '\0')
-		dl++;
+	//check message length (number of chars)
+	unsigned long long int len = 0;
+	while (ptr[len] != '\0')
+		len++;
 	//
 
-	if (dl * 4 > 0xffffffff)//jesli wiadomosc jest dluzsza niz (2^32)-1 bitow
+	if (len * 4 > 0xffffffff) //if message is longer than (2^32)-1 bits
 	{
-		std::cout << "BLAD! Wiadomosc zbyt dluga." << std::endl;
+		std::cout << "ERROR! Message is too long." << std::endl;
 		return 0;
 	}
 
-	if (dl <= 111)//dla wiadomosci do 111 znakow - jeden przebieg
+	if (len <= 111) //for messages that are not longer than 111 chars - one iteration
 	{
-		t[0] = dl * 4;//ustawiam licznik
+		t[0] = len * 4; //set counter
 
-		ladujWiadomosc(m, wsk, dl);
+		loadMsg(m, ptr, len);
 
 		//padding
-		m[dl / 8] << 4;
-		if (dl < 111)
+		m[len / 8] << 4;
+		if (len < 111)
 		{
-			m[dl / 8] += 8;
-			m[dl / 8] = m[dl / 8] << ((dl / 8 + 1) * 8 - dl - 1) * 4;
+			m[len / 8] += 8;
+			m[len / 8] = m[len / 8] << ((len / 8 + 1) * 8 - len - 1) * 4;
 			m[13] += 1;
 		}
 		else
-			m[dl / 8] += 9;
+			m[len / 8] += 9;
 		//
 	
 		m[14] = t[1];
 		m[15] = t[0];
 
-		Inicjalizacja(v, h, t);
-		Rundy(v, m);
-		Finalizacja(v, h);
-		wyswietlWynik(h);
+		initialization(v, h, t);
+		rounds(v, m);
+		finalization(v, h);
+		showResult(h);
 	}
 
-	if (dl > 111)//dla wiadomosci dluzszych niz 111 znakow
+	if (len > 111) //for messages that longer than 111 chars
 	{
-		unsigned long dlp = dl;//pozostala dlugosc
-		unsigned long t0prev;//poprzedni licznik
-		int przebieg = 1;
-		bool sflag = 0;//gdy wiadomosc ma dlugosc rowna wielokrotnosci 128 - specjalny przypadek
+		unsigned long rlen = len; //remaining length
+		unsigned long t0prev; //previous counter
+		int itr = 1; // iteration
+		bool sflag = 0; //if length of message is multiple of 128 - special case
 
-		//ustawiam licznik
-		if (dl <= 128)
-			t[0] = dl * 4;
+		//set counter
+		if (len <= 128)
+			t[0] = len * 4;
 		else
 			t[0] = 128 * 4;
 
-		while (dlp > 0)
+		while (rlen > 0)
 		{
-			ladujWiadomosc(m, wsk, dlp<=128 ? dlp : 128);
+			loadMsg(m, ptr, rlen<=128 ? rlen : 128);
 
-			if (dlp <= 111)
+			if (rlen <= 111)
 			{
 				//padding
-				m[dlp / 8] << 4;
-				if (dlp < 111)
+				m[rlen / 8] << 4;
+				if (rlen < 111)
 				{
-					m[dlp / 8] += 8;
-					m[dlp / 8] = m[dlp / 8] << ((dlp / 8 + 1) * 8 - dlp - 1) * 4;
+					m[rlen / 8] += 8;
+					m[rlen / 8] = m[rlen / 8] << ((rlen / 8 + 1) * 8 - rlen - 1) * 4;
 					m[13] += 1;
 				}
 				else
-					m[dlp / 8] += 9;
+					m[rlen / 8] += 9;
 				//
 				
 				m[14] = t[1];
-				m[15] = dl*4;
+				m[15] = len*4;
 
-				//ew. reset licznika
-				if (przebieg >= 2)
+				//possible conter reset
+				if (itr >= 2)
 				{
 					if (t0prev == t[0])
 						t[0] = 0;
@@ -225,29 +226,29 @@ int main(int argc, char *argv[])
 
 				t0prev = t[0];
 
-				Inicjalizacja(v, h, t);
-				Rundy(v, m);
-				Finalizacja(v, h);
-				wyswietlWynik(h);
+				initialization(v, h, t);
+				rounds(v, m);
+				finalization(v, h);
+				showResult(h);
 				return 0;
 			}
 
-			else if (dlp < 128)
+			else if (rlen < 128)
 			{
 				//padding
-				m[dlp / 8] << 4;
-				m[dlp / 8] += 8;
-				m[dlp / 8] = m[dlp / 8] << ((dlp / 8 + 1) * 8 - dlp - 1) * 4;
+				m[rlen / 8] << 4;
+				m[rlen / 8] += 8;
+				m[rlen / 8] = m[rlen / 8] << ((rlen / 8 + 1) * 8 - rlen - 1) * 4;
 
-				for (int i = 1; dlp / 8 + i <= 15; i++)
-					m[dlp / 8 + i] = 0;					
+				for (int i = 1; rlen / 8 + i <= 15; i++)
+					m[rlen / 8 + i] = 0;					
 			}
 
-			if (dlp == 128)
+			if (rlen == 128)
 				sflag = 1;
 			
-			//ew. reset licznika
-			if (przebieg >= 2)
+			//possible counter reset
+			if (itr >= 2)
 			{
 				if (t0prev == t[0])
 					t[0] = 0;
@@ -255,48 +256,48 @@ int main(int argc, char *argv[])
 
 			t0prev = t[0];
 
-			Inicjalizacja(v, h, t);
-			Rundy(v, m);
-			Finalizacja(v, h);
+			initialization(v, h, t);
+			rounds(v, m);
+			finalization(v, h);
 
-			//zerowanie m przed kolejnym przebiegiem
+			//reset m before next iteration
 			for (int i = 0; i <= 15; i++)
 				m[i] = 0;
-			przebieg++;
+			itr++;
 
-			//zmiana dlp na pozostala ilosc znakow i przesuniecie wsk
-			if (dlp >= 128)
-				dlp -= 128;
+			//change rlen to remaining chars number and move pointer ptr
+			if (rlen >= 128)
+				rlen -= 128;
 			else
-				dlp = 0;
-			wsk += 128;
+				rlen = 0;
+			ptr += 128;
 
-			//zwiekszenie licznika
-			if (dlp >= 128)
+			//increase the counter
+			if (rlen >= 128)
 				t[0] += 128 * 4;
 			else
-				t[0] = dl * 4;
+				t[0] = len * 4;
 		}
 		
-		//padding cd.
+		//padding countinued
 		if (sflag == 1)
 			m[0] = 0x80000000;
 
 		m[13] = 1;
 		m[14] = t[1];
-		m[15] = dl*4;
+		m[15] = len*4;
 
-		//ew. reset licznika
-		if (przebieg >= 2)
+		//possible counter reset
+		if (itr >= 2)
 		{
 			if (t0prev == t[0])
 				t[0] = 0;
 		}
 
-		Inicjalizacja(v, h, t);
-		Rundy(v, m);
-		Finalizacja(v, h);
-		wyswietlWynik(h);
+		initialization(v, h, t);
+		rounds(v, m);
+		finalization(v, h);
+		showResult(h);
 	}
 
 	return 0;
